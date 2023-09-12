e8d0f99f9a3a71a053fae36f336cd89a69aaa941
khurshid <ernazarovkh@gmail.com>
2023-09-12 11:55:38 +0200

    feat(bar): add left/right numbers to bars

--- image/bar.js
+++ image/bar.js
@@ -5,6 +5,8 @@ class StackBar {
   // rectangles
   recs = undefined;
   bars = undefined;
+  lefts = undefined;
+  rights = undefined;
   current = undefined;
 
   constructor() {
@@ -17,12 +19,36 @@ class StackBar {
       var tmp_bar = document.createElement("div");
       tmp_bar.classList.add("bar");
 
+      var tmp_bar_left = document.createElement("div");
+      tmp_bar_left.classList.add("bar-left");
+
+      var tmp_bar_left_content = document.createElement("div");
+      tmp_bar_left_content.classList.add("bar-left-content");
+      if (i + 1 < 10) {
+        tmp_bar_left_content.innerHTML = "0" + (i + 1);
+      } else {
+        tmp_bar_left_content.innerHTML = i + 1;
+      }
+
+      var tmp_bar_right = document.createElement("div");
+      tmp_bar_right.classList.add("bar-right");
+
+      var tmp_bar_right_content = document.createElement("div");
+      tmp_bar_right_content.classList.add("bar-right-content");
+      tmp_bar_right_content.innerHTML = data.length;
+
+      tmp_bar_left.appendChild(tmp_bar_left_content);
+      tmp_bar_right.appendChild(tmp_bar_right_content);
+      tmp_rec.appendChild(tmp_bar_left);
       tmp_rec.appendChild(tmp_bar);
+      tmp_rec.appendChild(tmp_bar_right);
       stack.appendChild(tmp_rec);
     }
 
     this.recs = document.querySelectorAll(".rectangle");
-    console.log(this.recs);
+    this.lefts = document.querySelectorAll(".bar-left");
+    this.rights = document.querySelectorAll(".bar-right");
+
     this.bars = [];
 
     for (var i = 0; i < this.recs.length; i++) {
@@ -36,44 +62,55 @@ class StackBar {
   }
 
   openIndex(idx) {
-    if (idx != this.current) {
-      this.closeIndex();
+    // if (idx != this.current) {
+    this.closeIndex();
 
-      let value = {
-        rec: 9,
-        bar: 2,
-        margin: 0,
-      };
+    let value = {
+      rec: 9,
+      bar: 2,
+      l_c: 0,
+      r_c: 0,
+    };
 
-      gsap.to(value, 0.24, {
-        rec: 108,
-        bar: 28,
-        ease: "power1.easeOut",
-        onUpdate: () => {
-          this.recs[idx].style.width = `${value.rec}px`;
-          this.bars[idx].style.width = `${value.bar}px`;
-        },
-        onComplete: () => {
-          this.current = idx;
-        },
-      });
-    }
+    gsap.to(value, 0.24, {
+      rec: 98,
+      bar: 28,
+      l_c: 25,
+      r_c: 25,
+      ease: "power1.easeOut",
+      onUpdate: () => {
+        this.recs[idx].style.width = `${value.rec}px`;
+        this.bars[idx].style.width = `${value.bar}px`;
+        this.lefts[idx].style.width = `${value.l_c}px`;
+        this.rights[idx].style.width = `${value.r_c}px`;
+      },
+      onComplete: () => {
+        this.current = idx;
+      },
+    });
+    // }
   }
 
   closeIndex() {
     if (this.current != undefined) {
       let value = {
-        rec: 108,
+        rec: 98,
         bar: 28,
+        l_c: 25,
+        r_c: 25,
       };
 
       gsap.to(value, 0.24, {
         rec: 9,
         bar: 2,
+        l_c: 0,
+        r_c: 0,
         ease: "power1.easeOut",
         onUpdate: () => {
           this.recs[this.current].style.width = `${value.rec}px`;
           this.bars[this.current].style.width = `${value.bar}px`;
+          this.lefts[this.current].style.width = `${value.l_c}px`;
+          this.rights[this.current].style.width = `${value.r_c}px`;
         },
       });
     }
